0a67516003d750a7a6d85bc5285505e0da7ca3aa
Athu <athu57@hotmail.dk>
2020-11-03 22:43:55 +0100

    SignIn form created with email and pw input aswell as sign in btn
    Sign in page created and ready to be designed.
    Validation has been made on the textfields so invalid inputs wont be send to firebase. 
    TextSpan needs to redirect to Register() widget.

---
+++ share_goods/lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:share_goods/myColors.dart';
+import 'package:share_goods/myAppBar.dart';
+import 'package:share_goods/screens/authenticate/register.dart';
+import 'package:share_goods/services/auth.dart';
+import 'package:flutter/gestures.dart';
+
+
+
+class SignIn extends StatefulWidget {
+  final Function togglePage;
+  SignIn({this.togglePage});
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  // instance of AuthService class auth from services/auth.dart
+  final AuthService _auth = AuthService();
+
+  // key used for making clickable TextSpan
+  final GlobalKey _textKey = new GlobalKey();
+
+  final _formKey = GlobalKey<FormState>();
+
+  // text field variables
+  String email ='';
+  String password = '';
+  String error = '';
+
+  // text styles
+  TextStyle defaultStyle = TextStyle(color: Colors.grey, fontSize: 9.0);
+  TextStyle linkStyle = TextStyle(color: myLightGreen, fontSize: 10.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar(
+        title: 'Sign in',
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 75.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+
+              SizedBox(height: 20.0),
+              TextFormField(
+                  decoration: InputDecoration(
+                      errorStyle: TextStyle(
+                        fontSize: 8.0,
+                      )
+                  ),
+                  validator: (val) => !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? 'Enter valid email' : null,
+                onChanged: (val) {
+                  setState(() {
+                    email = val;
+                  });
+                }
+              ),
+
+              SizedBox(height: 20.0),
+              TextFormField(
+                  decoration: InputDecoration(
+                      errorStyle: TextStyle(
+                        fontSize: 8.0,
+                      )
+                  ),
+                  validator: (val) => val.length < 6 ? 'Password must be atleast 6 chars long' : null,
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  }
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ButtonTheme(
+                  minWidth: 100.0,
+                  height: 40.0,
+                  child: RaisedButton(
+                      color: myLightGreen,
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(color: Colors.white, ),
+                      ),
+                      onPressed: () async {
+                        print('Email is:' + email);
+                        print('Password is' + password);
+                      }
+                  ),
+                ),
+              ),
+
+              RichText(
+                key: _textKey,
+                text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Sign in or choose to ',
+                      style: defaultStyle,
+                      recognizer: TapGestureRecognizer()..onTap = (){
+                        print('Clicked Sign in');
+                    }
+                  ),
+                  TextSpan(
+                      text: 'Register',
+                      style: linkStyle,
+                      recognizer: TapGestureRecognizer()..onTap = () {
+                        widget.togglePage();
+                      print('Clicked Register');
+                      setState(() {
+                        Register();
+                      });
+                    },
+                  )
+                ]
+              ))
+             ],
+          )
+        )
+      ),
+    );
+  }
+}
