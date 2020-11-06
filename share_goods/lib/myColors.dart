21b3ba66c22a2fd785e4c594dbfdfd13cd1512ed
Athu <athu57@hotmail.dk>
2020-11-06 04:16:30 +0100

    New sign_in widget with new design
    Created new sign_in widget design and code. 
    Added gradient background, new graphics and buttons.
    Implemented the old login and register logic aswell.

--- share_goods/lib/myColors.dart
+++ share_goods/lib/myColors.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color myLightGreen = Color(0xFF76DEBB);
+Color myGradientGreen1 = Colors.green[300];
+Color myGradientGreen2 = Colors.green[400];
 Color myDarkGreen = Color(0xFF26473D);
+Color myGradientGreen0 = Colors.green[200];
--- share_goods/lib/screens/authenticate/sign_in.dart
+++ share_goods/lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:share_goods/myColors.dart';
 import 'package:share_goods/myAppBar.dart';
@@ -32,122 +34,237 @@ class _SignInState extends State<SignIn> {
   String error = '';
 
   // text styles
-  TextStyle defaultStyle = TextStyle(color: Colors.grey, fontSize: 9.0);
-  TextStyle linkStyle = TextStyle(color: myLightGreen, fontSize: 10.0);
+  TextStyle defaultStyle = TextStyle(color: Colors.white, fontSize: 10.0);
+  TextStyle linkStyle = TextStyle(color: myLightGreen, fontSize: 12.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(
-        title: 'Sign in',
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 75.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: InputDecoration(
-                      errorStyle: TextStyle(
-                        fontSize: 8.0,
-                      ),
-                    hintText: 'Email'
-                  ),
-                  validator: (val) => !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? 'Enter valid email' : null,
-                onChanged: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                }
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    myGradientGreen0,
+                    myGradientGreen1,
+                    myDarkGreen,
+                  ],
+                  stops: [0.1, 0.6, 0.9],
+                )
               ),
-
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: InputDecoration(
-                      errorStyle: TextStyle(
-                        fontSize: 8.0,
-                      ),
-                    hintText: 'Password'
+            ),
+            Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 40.0,
+                  vertical: 120.0,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Welcome to',style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 20.0, fontWeight: FontWeight.normal),),
+                      Text('ShareGoods', style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 30.0, fontWeight: FontWeight.bold),),
+                      SizedBox(height: 30.0),
+                      _buildEmailTextField(),
+                      SizedBox(height: 30.0,),
+                      _buildPasswordTextField(),
+                      _buildForgotPassword(),
+                      _buildLoginButton(),
+                      _buildRegisterButton(),
+                    ],
                   ),
-                  validator: (val) => val.length < 6 ? 'Password must be atleast 6 chars long' : null,
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() {
-                      password = val;
-                    });
-                  }
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ButtonTheme(
-                  minWidth: 100.0,
-                  height: 40.0,
-                  child: RaisedButton(
-                      color: myLightGreen,
-                      child: Text(
-                        'Sign in',
-                        style: TextStyle(color: Colors.white, ),
-                      ),
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          dynamic result = await _auth.signInMail(email.trim(),
-                              password.trim());
-                          print('Result is' + result);
-                          if (result == null) {
-                            print('Result was null');
-                          } else {
-                            setState(() => error = 'Enter valid input');
-                          }
-                          print('Email is:' + email);
-                          print('Password is' + password);
-                        }
-                      }
-                  ),
-
                 ),
               ),
+            )
+          ]
 
-              SizedBox(height: 12.0),
-              Text(
-                  error,
-                  style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 12.0
-                  )
+        ),
+      )
+    )
+    ;
+  }
+
+  Widget _buildEmailTextField() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+      Text(
+        'Email',
+        style: TextStyle(color: Colors.white),
+      ),
+      SizedBox(height: 10.0),
+      Container(
+        alignment: Alignment.centerLeft,
+        height: 60,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: myGradientGreen1,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10.0,
+                offset: Offset(0,3),
+              )
+            ]
+        ),
+        child: TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 15),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
               ),
+              hintText: 'Enter your E-mail',
+              hintStyle: TextStyle(color: Colors.white, fontFamily: 'OpenSans')
+          ),
+            validator: (val) => !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? 'Enter valid email' : null,
+            onChanged: (val) {
+              setState(() {
+                email = val;
+              });
+            }
+        ),
+      )
+    ],
+    );
+  }
 
-              RichText(
-                key: _textKey,
-                text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                      text: 'Sign in or choose to ',
-                      style: defaultStyle,
-                      recognizer: TapGestureRecognizer()..onTap = (){
-                        print('Clicked Sign in');
-                    }
-                  ),
-                  TextSpan(
-                      text: 'Register',
-                      style: linkStyle,
-                      recognizer: TapGestureRecognizer()..onTap = () {
-                      widget.togglePage();
-                      print('Clicked Register');
-                      //setState(() {
-                        //widget.togglePage();
-                      //});
-                    },
-                  )
-                ]
-              ))
-             ],
-          )
+  Widget _buildPasswordTextField() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+      Text(
+        'Password',
+        style: TextStyle(color: Colors.white),
+      ),
+      SizedBox(height: 10.0),
+      Container(
+        alignment: Alignment.centerLeft,
+        height: 60,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: myGradientGreen1,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 6.0,
+                offset: Offset(0,5),
+              )
+            ]
+        ),
+        child: TextFormField(
+          keyboardType: TextInputType.text,
+          obscureText: true,
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 15),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Password',
+              hintStyle: TextStyle(color: Colors.white, fontFamily: 'OpenSans')
+          ),
+            validator: (val) => val.length < 6 ? 'Password must be atleast 6 chars long' : null,
+            onChanged: (val) {
+              setState(() {
+                password = val;
+              });
+            }
+        ),
+      )
+    ],
+    );
+  }
+
+  Widget _buildForgotPassword() {
+    return Container(
+        alignment: Alignment.centerRight,
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0)
+          ),
+          onPressed: () => print('Forgot pw Button Pressed'),
+          padding: EdgeInsets.only(right: 0.0),
+          child: Text(
+            'Forgot Password?',
+            style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 12.0),
+          ),
         )
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      child: ButtonTheme(
+        minWidth: double.maxFinite,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0)
+        ),
+        child: RaisedButton(
+            color: Colors.white,
+            elevation: 5.0,
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              'LOGIN',
+              style: TextStyle(color: Color(0xFF527DAA), letterSpacing: 1.5, fontSize: 18.0, fontWeight: FontWeight.bold, fontFamily: 'OpenSans' ),
+            ),
+            onPressed: () async {
+              if (_formKey.currentState.validate()) {
+                dynamic result = await _auth.signInMail(email.trim(),
+                    password.trim());
+                print('Result is' + result);
+                if (result == null) {
+                  print('Result was null');
+                } else {
+                  setState(() => error = 'Enter valid input');
+                }
+                print('Email is:' + email);
+                print('Password is' + password);
+              }
+            }
+        ),
       ),
     );
   }
+
+  Widget _buildRegisterButton() {
+    return RichText(
+        key: _textKey,
+        text: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                  text: 'No account? ',
+                  style: defaultStyle,
+                  recognizer: TapGestureRecognizer()..onTap = (){
+                    print('Clicked Sign in');
+                  }
+              ),
+              TextSpan(
+                text: 'Register',
+                style: linkStyle,
+                recognizer: TapGestureRecognizer()..onTap = () {
+                  widget.togglePage();
+                  print('Clicked Register');
+                  //setState(() {
+                  //widget.togglePage();
+                  //});
+                },
+              )
+            ]
+        ));
+  }
+
 }
