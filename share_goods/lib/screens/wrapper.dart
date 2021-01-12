4a9cbfb46e5285ecba671c77fee9d6bc61d244a7
Athu <athu57@hotmail.dk>
2021-01-12 14:27:20 +0100

    Fixedx typo in comment

--- share_goods/lib/screens/wrapper.dart
+++ share_goods/lib/screens/wrapper.dart
@@ -10,8 +10,9 @@ import 'package:share_goods/screens/home/home.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   // Specify we need user from Stream - accessing user data from Provider in main.dart everytime there's a new valuie
+   // Specify we need user from Stream - accessing user data from Provider in main.dart everytime there's a new value
     final user = Provider.of<LocalUser>(context);
+
     print("user er $user");
     //return either Home or Authenticate based on if user is signed in or not.
     if(user == null){
