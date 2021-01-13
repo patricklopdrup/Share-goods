75e032e31115511bf781802b330e896bbed7e463
Martin Rylund <martin@rylund.eu>
2021-01-13 15:45:09 +0100

    Added shouldBuy to Item class

--- share_goods/lib/models/item.dart
+++ share_goods/lib/models/item.dart
@@ -3,17 +3,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Item {
   final String name;
   final int timesBought;
+  final bool shouldBuy;
   final DocumentReference reference;
 
   Item.fromMap(Map<String, dynamic> map, {this.reference})
     : assert(map['name'] != null),
       assert(map['timesBought'] != null),
+      assert(map['shouldBuy'] != null),
       name = map['name'],
-      timesBought = map['timesBought'];
+      timesBought = map['timesBought'],
+      shouldBuy = map['shouldBuy'];
 
   Item.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data(), reference: snapshot.reference);
 
   @override
-  String toString() => "Record<$name>";
+  String toString() => "Item<$name>";
 }
