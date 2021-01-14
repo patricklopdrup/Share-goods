897a5fe96e1f644f6fe14bee80ebaa8186cd67c9
Martin Rylund <martin@rylund.eu>
2021-01-14 01:47:34 +0100

    Cleaned up listItem and added documentation

--- share_goods/lib/screens/itemScreen/widgets/listItem.dart
+++ share_goods/lib/screens/itemScreen/widgets/listItem.dart
@@ -6,152 +6,224 @@ class ItemListItemWidget extends StatelessWidget {
 
   final Item item;
 
+  /// Initiate build from an [Item] object
+  ///
+  /// Check if the item is needed or an item from the
+  /// inventory and call the corresponding methods.
   @override
   Widget build(BuildContext context) {
     if (item.shouldBuy) {
-      return _BuildNeedCard(context);
+      return _buildNeedCard(context);
     } else {
-      return _BuildInventoryCard(context);
+      return _buildInventoryCard(context);
     }
   }
 
-  Widget _BuildNeedCard(BuildContext context) {
+  /// Build a Card from items that are needed
+  Widget _buildNeedCard(BuildContext context) {
     return Card(
       elevation: 0,
       margin: new EdgeInsets.fromLTRB(15, 6, 25, 0),
       color: Colors.red.withOpacity(0),
       child: Container(
-          height: 55,
-          child: Stack(children: [
-            Row(children: [
-              Expanded(
-                flex: 6,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(255, 255, 255, 1),
-                      borderRadius:
-                          BorderRadius.only(bottomLeft: Radius.circular(10)),
-                      border: Border.all(color: Colors.black)),
-                  height: 45,
-                  width: 50,
-                  margin: EdgeInsets.only(
-                    left: 10,
-                    top: 10,
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(item.name, style: TextStyle(fontSize: 22)),
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10, left: 10),
-                  child: Ink(
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 1),
-                        borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(10),
-                            bottomRight: Radius.circular(10)),
-                        border: Border.all(color: Colors.black)),
-                    child: InkWell(
-                      customBorder: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(10),
-                              bottomRight: Radius.circular(10))),
-                      onTap: () => item.reference.update({
-                        'shouldBuy': !item.shouldBuy,
-                        'timesBought': item.timesBought + 1
-                      }),
-                      child: Container(
-                        height: 45,
-                        width: 45,
-                        alignment: Alignment.center,
-                        child: Text('Køb',
-                            style: TextStyle(
-                                fontSize: 18, color: Colors.lightGreen)),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ]),
-            Container(
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(118, 222, 187, 1),
-                    shape: BoxShape.circle),
-                width: 30,
-                height: 30,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "!",
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
-                  ),
-                )),
-          ])),
+        height: 55,
+        child: Stack(children: [
+          Row(children: [
+            _buildNeedItemBanner(context),
+            _buildNeedBuyButton(context),
+          ]),
+          _buildNeedCircle(context),
+        ]),
+      ),
     );
   }
 
-
-  Widget _BuildInventoryCard(BuildContext context) {
-    return Card(
-      elevation: 0,
-      margin: new EdgeInsets.fromLTRB(25, 10, 25, 0),
-      color: Colors.red.withOpacity(0),
-      child: Row(children: [
-        Expanded(
-          flex: 6,
-          child: Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(255, 255, 255, 1),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    topLeft: Radius.circular(10)),
-                border: Border.all(color: Colors.black)),
-            height: 45,
-            width: 50,
-            alignment: Alignment.center,
-            child: Text(item.name, style: TextStyle(fontSize: 22)),
-          ),
+  /// Build the container with the item name within it
+  Widget _buildNeedItemBanner(BuildContext context) {
+    return Expanded(
+      flex: 6,
+      child: Container(
+        height: 45,
+        width: 50,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10)),
+          border: Border.all(color: Colors.black),
         ),
-        Expanded(
-          flex: 3,
-          child: Padding(
-            padding: EdgeInsets.only(top: 0, left: 10),
-            child: Ink(
-              decoration: BoxDecoration(
-                  color: Color.fromRGBO(255, 255, 255, 1),
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
-                  border: Border.all(color: Colors.black)),
-              child: InkWell(
-                customBorder: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(10),
-                        bottomRight: Radius.circular(10))),
-                onTap: () => item.reference.update({
+        margin: EdgeInsets.only(
+          left: 10,
+          top: 10,
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          item.name,
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+
+  /// Build the button you press to mark the item as bought
+  ///
+  /// We use [Ink] around [InkWell] to make the ripple effect
+  /// when clicking the container appear on top, otherwise it would
+  /// be invisible.
+  Widget _buildNeedBuyButton(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: Padding(
+        padding: EdgeInsets.only(top: 10, left: 10),
+        child: Ink(
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(255, 255, 255, 1),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            border: Border.all(color: Colors.black),
+          ),
+          child: InkWell(
+            customBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+            ),
+            onTap: () {
+              Future.delayed(Duration(milliseconds: 150), () {
+                item.reference.update({
                   'shouldBuy': !item.shouldBuy,
                   'timesBought': item.timesBought + 1
-                }),
-                child: Container(
-                  height: 45,
-                  width: 45,
-                  margin: EdgeInsets.only(
-                    left: 10,
-                  ),
-                  alignment: Alignment.center,
-                  child: Text('Mangler',
-                      style: TextStyle(fontSize: 18, color: Colors.redAccent)),
+                });
+              });
+            },
+            child: Container(
+              height: 45,
+              width: 45,
+              alignment: Alignment.center,
+              child: Text(
+                'Køb',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.lightGreen,
                 ),
               ),
             ),
           ),
         ),
-      ]),
+      ),
+    );
+  }
+
+  /// Build the small circle with the exclamation mark in it
+  Widget _buildNeedCircle(BuildContext context) {
+    return Container(
+      width: 30,
+      height: 30,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(118, 222, 187, 1),
+        shape: BoxShape.circle,
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          "!",
+          style: TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Build a Card from items that are in the inventory
+  Widget _buildInventoryCard(BuildContext context) {
+    return Card(
+      elevation: 0,
+      margin: new EdgeInsets.fromLTRB(25, 10, 25, 0),
+      color: Colors.red.withOpacity(0),
+      child: Row(
+        children: [
+          _buildInventoryItemBanner(context),
+          _buildInventoryNeedButton(context),
+        ],
+      ),
+    );
+  }
+
+  /// Build the container with the item name within it
+  Widget _buildInventoryItemBanner(BuildContext context) {
+    return Expanded(
+      flex: 6,
+      child: Container(
+        height: 45,
+        width: 50,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            topLeft: Radius.circular(10),
+          ),
+          border: Border.all(color: Colors.black),
+        ),
+        child: Text(
+          item.name,
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+
+  /// Build the button you press to mark the item as bought
+  ///
+  /// Pretty much the same as [_buildNeedBuyButton] just fine tuned
+  /// to fit another sizing layout.
+  Widget _buildInventoryNeedButton(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: Padding(
+        padding: EdgeInsets.only(top: 0, left: 10),
+        child: Ink(
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(255, 255, 255, 1),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+            border: Border.all(color: Colors.black),
+          ),
+          child: InkWell(
+            customBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+            ),
+            onTap: () => item.reference.update({
+              'shouldBuy': !item.shouldBuy,
+              'timesBought': item.timesBought + 1
+            }),
+            child: Container(
+              height: 45,
+              width: 45,
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(
+                left: 10,
+              ),
+              child: Text(
+                'Mangler',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.redAccent,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
