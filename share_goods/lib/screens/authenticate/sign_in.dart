3c67313d199730a4a574f38fc58eee1ba4c5bd93
Athu <athu57@hotmail.dk>
2021-01-14 03:40:58 +0100

    Reformatted code

--- share_goods/lib/screens/authenticate/sign_in.dart
+++ share_goods/lib/screens/authenticate/sign_in.dart
@@ -15,7 +15,6 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-
   // instance of AuthService class auth from services/auth.dart
   final AuthService _auth = AuthService();
 
@@ -24,14 +23,13 @@ class _SignInState extends State<SignIn> {
   final _formKey = GlobalKey<FormState>();
 
   // text field variables
-  String email ='';
+  String email = '';
   String password = '';
   String error = '';
   bool checkS = false;
   // text styles
   TextStyle defaultStyle = TextStyle(color: Colors.white, fontSize: 10.0);
   TextStyle linkStyle = TextStyle(color: myLightGreen, fontSize: 12.0);
-
 
   @override
   Widget build(BuildContext context) {
@@ -50,18 +48,19 @@ class _SignInState extends State<SignIn> {
         ),
         child: Scaffold(
             backgroundColor: Colors.transparent,
-          body: GestureDetector(
-            onTap: (){
-              FocusScope.of(context).unfocus();
-              checkS = false;
-              error = '';
-            },
-            child: Stack(
-              children: <Widget>[
+            body: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+                checkS = false;
+                error = '';
+              },
+              child: Stack(children: <Widget>[
                 Container(
                   height: double.infinity,
                   child: SingleChildScrollView(
-                    physics: checkS ? const AlwaysScrollableScrollPhysics() : const NeverScrollableScrollPhysics(),
+                    physics: checkS
+                        ? const AlwaysScrollableScrollPhysics()
+                        : const NeverScrollableScrollPhysics(),
                     padding: EdgeInsets.symmetric(
                       horizontal: 40.0,
                       vertical: 100.0,
@@ -71,15 +70,38 @@ class _SignInState extends State<SignIn> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          Text('Velkommen til ',style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 20.0, fontWeight: FontWeight.normal),),
-                          Text('ShareGoods', style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 30.0, fontWeight: FontWeight.bold),),
+                          Text(
+                            'Velkommen til ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'OpenSans',
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          Text(
+                            'ShareGoods',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'OpenSans',
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold),
+                          ),
                           SizedBox(height: 30.0),
                           _buildEmailTextField(),
-                          SizedBox(height: 30.0,),
+                          SizedBox(
+                            height: 30.0,
+                          ),
                           _buildPasswordTextField(),
                           _buildForgotPassword(),
                           SizedBox(height: 5.0),
-                          Text(error, style: TextStyle(color: Colors.red, fontSize: 12, fontFamily: 'OpenSans'),),
+                          Text(
+                            error,
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 12,
+                                fontFamily: 'OpenSans'),
+                          ),
                           _buildLoginButton(),
                           _buildRegisterButton(),
                         ],
@@ -87,119 +109,120 @@ class _SignInState extends State<SignIn> {
                     ),
                   ),
                 )
-              ]
-          ),
-        )
-      )
-    );
+              ]),
+            )));
   }
 
   Widget _buildEmailTextField() {
     final node = FocusScope.of(context);
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-      Text(
-        'Mail',
-        style: TextStyle(color: Colors.white),
-      ),
-      SizedBox(height: 10.0),
-      Container(
-        alignment: Alignment.centerLeft,
-        height: 60,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(10.0),
-            color: myGradientGreen1,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                blurRadius: 10.0,
-                offset: Offset(0,3),
-              )
-            ]
-        ),
-        child: TextFormField(
-          textInputAction: TextInputAction.next,
-          onEditingComplete: () => node.nextFocus(),
-          keyboardType: TextInputType.emailAddress,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Mail',
           style: TextStyle(color: Colors.white),
-          decoration: InputDecoration(
-            errorStyle: TextStyle(height: 0) ,
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 15),
-              prefixIcon: Icon(
-                Icons.email,
-                color: Colors.white,
-              ),
-              hintText: 'Indtast din mail',
-              hintStyle: TextStyle(color: Colors.white, fontFamily: 'OpenSans')
-          ),
-          onTap: (){
-            checkS = true;
-          },
-          validator: (val) => !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? '' : null,
-          onChanged: (val) {
-            setState(() {
-              email = val;
-            });
-          }
         ),
-      )
-    ],
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60,
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(10.0),
+              color: myGradientGreen1,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10.0,
+                  offset: Offset(0, 3),
+                )
+              ]),
+          child: TextFormField(
+              textInputAction: TextInputAction.next,
+              onEditingComplete: () => node.nextFocus(),
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  errorStyle: TextStyle(height: 0),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 15),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                  hintText: 'Indtast din mail',
+                  hintStyle:
+                      TextStyle(color: Colors.white, fontFamily: 'OpenSans')),
+              onTap: () {
+                checkS = true;
+              },
+              validator: (val) =>
+                  !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(val)
+                      ? ''
+                      : null,
+              onChanged: (val) {
+                setState(() {
+                  email = val;
+                });
+              }),
+        )
+      ],
     );
   }
 
   Widget _buildPasswordTextField() {
     final node = FocusScope.of(context);
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-      Text(
-        'Kodeord',
-        style: TextStyle(color: Colors.white),
-      ),
-      SizedBox(height: 10.0),
-      Container(
-        alignment: Alignment.centerLeft,
-        height: 60,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            border: Border.all(color: Colors.white),
-            color: myGradientGreen1,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                blurRadius: 6.0,
-                offset: Offset(0,5),
-              )
-            ]
-        ),
-        child: TextFormField(
-         textInputAction: TextInputAction.done,
-            onFieldSubmitted: (_) => node.unfocus(),
-          keyboardType: TextInputType.text,
-          obscureText: true,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Kodeord',
           style: TextStyle(color: Colors.white),
-          decoration: InputDecoration(
-            errorStyle: TextStyle(height: 0),
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 15),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: Colors.white,
-              ),
-              hintText: 'Indtast kodeord',
-              hintStyle: TextStyle(color: Colors.white, fontFamily: 'OpenSans')
-          ),
-            onTap: (){
-              checkS = true;
-            },
-            validator: (val) => val.length < 6 ? '' : null,
-            onChanged: (val) {
-              setState(() {
-                password = val;
-              });
-            }
         ),
-      )
-    ],
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.white),
+              color: myGradientGreen1,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 6.0,
+                  offset: Offset(0, 5),
+                )
+              ]),
+          child: TextFormField(
+              textInputAction: TextInputAction.done,
+              onFieldSubmitted: (_) => node.unfocus(),
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  errorStyle: TextStyle(height: 0),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 15),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.white,
+                  ),
+                  hintText: 'Indtast kodeord',
+                  hintStyle:
+                      TextStyle(color: Colors.white, fontFamily: 'OpenSans')),
+              onTap: () {
+                checkS = true;
+              },
+              validator: (val) => val.length < 6 ? '' : null,
+              onChanged: (val) {
+                setState(() {
+                  password = val;
+                });
+              }),
+        )
+      ],
     );
   }
 
@@ -207,10 +230,9 @@ class _SignInState extends State<SignIn> {
     return Container(
         alignment: Alignment.centerRight,
         child: FlatButton(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0)
-          ),
-          onPressed: (){
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => ForgotPW()),
@@ -220,10 +242,10 @@ class _SignInState extends State<SignIn> {
           padding: EdgeInsets.only(right: 0.0),
           child: Text(
             'Glemt kodeord?',
-            style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 12.0),
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'OpenSans', fontSize: 12.0),
           ),
-        )
-    );
+        ));
   }
 
   Widget _buildLoginButton() {
@@ -231,9 +253,8 @@ class _SignInState extends State<SignIn> {
       padding: EdgeInsets.symmetric(vertical: 25.0),
       child: ButtonTheme(
         minWidth: double.maxFinite,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0)
-        ),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
         child: RaisedButton(
             color: Colors.white,
             highlightColor: myLightBlue4,
@@ -241,12 +262,17 @@ class _SignInState extends State<SignIn> {
             padding: EdgeInsets.all(15.0),
             child: Text(
               'LOGIN',
-              style: TextStyle(color: Color(0xFF527DAA), letterSpacing: 4.0, fontSize: 18.0, fontWeight: FontWeight.bold, fontFamily: 'OpenSans' ),
+              style: TextStyle(
+                  color: Color(0xFF527DAA),
+                  letterSpacing: 4.0,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'OpenSans'),
             ),
             onPressed: () async {
               if (_formKey.currentState.validate()) {
-                dynamic result = await _auth.signInMail(email.trim(),
-                    password.trim());
+                dynamic result =
+                    await _auth.signInMail(email.trim(), password.trim());
                 print('Result is' + result);
                 if (result == null) {
                   print('Result was null');
@@ -254,11 +280,10 @@ class _SignInState extends State<SignIn> {
                 }
                 print('Email is:' + email);
                 print('Password is' + password);
-              }else{
+              } else {
                 setState(() => error = 'Enter valid email or password');
               }
-            }
-        ),
+            }),
       ),
     );
   }
@@ -266,25 +291,23 @@ class _SignInState extends State<SignIn> {
   Widget _buildRegisterButton() {
     return RichText(
         key: _textKey,
-        text: TextSpan(
-            children: <TextSpan>[
-              TextSpan(
-                  text: 'No account? ',
-                  style: defaultStyle,
-                  recognizer: TapGestureRecognizer()..onTap = (){
-                    print('Clicked No Account');
-                  }
-              ),
-              TextSpan(
-                text: 'Register',
-                style: linkStyle,
-                recognizer: TapGestureRecognizer()..onTap = () {
-                  widget.togglePage();
-                  print('Clicked Register');
-                },
-              )
-            ]
-        ));
+        text: TextSpan(children: <TextSpan>[
+          TextSpan(
+              text: 'No account? ',
+              style: defaultStyle,
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  print('Clicked No Account');
+                }),
+          TextSpan(
+            text: 'Register',
+            style: linkStyle,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                widget.togglePage();
+                print('Clicked Register');
+              },
+          )
+        ]));
   }
-
 }
