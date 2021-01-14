ebcb34ac425c168413b0c4b2803604d8260a21a1
Martin Rylund <martin@rylund.eu>
2021-01-14 05:03:07 +0100

    (Martin & Athu) Fetched køkkener fra Firestore ud fra user data

--- share_goods/lib/pages_bottomnav/choose_kitchen_page.dart
+++ share_goods/lib/pages_bottomnav/choose_kitchen_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:share_goods/myAppBar.dart';
 import 'package:share_goods/myColors.dart';
@@ -22,29 +24,42 @@ class KitchenList extends StatefulWidget {
   _KitchenListState createState() => _KitchenListState();
 }
 
-class _KitchenListState extends State<KitchenList> {
-  final List kitchens = [
-    'Køkken L',
-    'Det seje køkken',
-    'Køkken hej',
-    // 'Køkken L',
-    // 'Det seje køkken',
-    // 'Køkken hej',
-    // 'Køkken L',
-    // 'Det seje køkken',
-    // 'Køkken hej'
-  ];
+getKitchens() async {
+  FirebaseAuth auth = FirebaseAuth.instance;
 
+  CollectionReference ref = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(auth.currentUser.uid)
+      .collection('kitchens');
+
+  QuerySnapshot userKitchens = await ref.get();
+  List<Map<String, Object>> kitchens = [];
+  userKitchens.docs.forEach((data) {
+    kitchens.add({'name': data.get('name'), 'kitchen': data.get('kitchen')});
+  });
+  return kitchens;
+}
+
+class _KitchenListState extends State<KitchenList> {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-          itemCount: kitchens.length,
-          itemBuilder: (context, index) {
-            return KitchenCard(title: this.kitchens[index]);
-          }),
-    );
-  }
+  Widget build(BuildContext context) => FutureBuilder(
+        future: getKitchens(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Map<String, Object>> kitchens = snapshot.data;
+            return Container(
+              child: ListView.builder(
+                  itemCount: kitchens.length,
+                  itemBuilder: (context, index) {
+                    return KitchenCard(title: kitchens[index]['name']);
+                  }),
+            );
+          }
+          return SizedBox(
+            height: 100,
+          );
+        },
+      );
 }
 
 // Single card for a kitchen
