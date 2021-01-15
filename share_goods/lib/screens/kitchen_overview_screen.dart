213ddf884adb3fcc63771498a28f89578a1ce3c6
Athu <athu57@hotmail.dk>
2021-01-15 21:19:25 +0100

    Added ClipPath Bezier Curve in Stack and made ListView Scrollable inside
    + Issues with ListView not being positioned right, and scroll indicator of ListView being bugged are now fixed. 
    
    + Added ClipPath Bezier Curve to page ( design changed aswell ).

--- share_goods/lib/screens/kitchen_overview_screen.dart
+++ share_goods/lib/screens/kitchen_overview_screen.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:share_goods/screens/shoppinglist_screen.dart';
+import 'package:share_goods/utils/Colors.dart';
 import 'package:share_goods/widgets/app_bar.dart';
 import 'package:share_goods/widgets/route_slide_animation.dart';
 import 'package:loading_animations/loading_animations.dart';
-
 
 class KitchenOverview extends StatefulWidget {
   @override
@@ -16,8 +17,66 @@ class _KitchenOverviewState extends State<KitchenOverview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: CustomAppBar(title: 'Mine Køkkener'),
-      body: _buildKitchenOverview(context),
+      body: Column(
+        children: [
+          Stack(children: <Widget>[
+            buildOpacityClipPath(context),
+            buildClipPath(context),
+          ]),
+          Expanded(
+              child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: _buildKitchenOverview(context)))
+        ],
+      ),
+    );
+  }
+
+  Widget buildClipPath(BuildContext context) {
+    return ClipPath(
+      clipper: ClippingClass(),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 3.6,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.centerRight,
+            stops: [0.1, 0.9],
+            colors: [
+              myGradientGreen1,
+              myGradientGreen2,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildOpacityClipPath(BuildContext context) {
+    return Opacity(
+      opacity: 0.75,
+      child: ClipPath(
+        clipper: ClippingClass(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height / 3.3,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.centerRight,
+              stops: [0.1, 0.9],
+              colors: [
+                myGradientGreen1,
+                myGradientGreen2,
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
@@ -54,21 +113,23 @@ class _KitchenOverviewState extends State<KitchenOverview> {
   }
 
   /// Build the list containing the info received from _buildPageViewBody
-  Widget _buildList(
-      BuildContext context, List<DocumentSnapshot> snapshot) {
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     final ScrollController _scrollController = ScrollController();
     return Scrollbar(
-      isAlwaysShown: true,
       controller: _scrollController,
-      child: ListView(
-          controller: _scrollController,
-          children: snapshot
-              .map((data) => KitchenCard(title: data.get('name'), ref: data.get('kitchen'),)
-              )
-              .toList()),
+      child: Container(
+        child: ListView(
+            padding: EdgeInsets.fromLTRB(0, 1, 0, 20),
+            controller: _scrollController,
+            children: snapshot
+                .map((data) => KitchenCard(
+                      title: data.get('name'),
+                      ref: data.get('kitchen'),
+                    ))
+                .toList()),
+      ),
     );
   }
-
 
   Widget _buildFuture(BuildContext context) {
     return FutureBuilder(
@@ -78,15 +139,19 @@ class _KitchenOverviewState extends State<KitchenOverview> {
           List<Map<String, Object>> kitchens = snapshot.data;
           return Container(
             child: ListView.builder(
+                shrinkWrap: true,
                 itemCount: kitchens.length,
                 itemBuilder: (context, index) {
-                  return KitchenCard(title: kitchens[index]['name'], ref: kitchens[index]['kitchen'],);
+                  return KitchenCard(
+                    title: kitchens[index]['name'],
+                    ref: kitchens[index]['kitchen'],
+                  );
                 }),
           );
         }
         return Align(
           alignment: Alignment.center,
-          child:  CircularProgressIndicator(),
+          child: CircularProgressIndicator(),
         );
       },
     );
@@ -105,13 +170,67 @@ class KitchenCard extends StatelessWidget {
     return ListTile(
       onTap: () {
         Future<DocumentSnapshot> hej = ref.get();
-        hej.then((value) =>
-            Navigator.push(context, SlidingPageChange(page: ShoppingList(kitchenDoc: value.reference, kitchenName: title,)))
-        );
+        hej.then((value) => Navigator.push(
+            context,
+            SlidingPageChange(
+                page: ShoppingList(
+              kitchenDoc: value.reference,
+              kitchenName: title,
+            ))));
       },
       leading: Icon(Icons.kitchen),
       title: Text(title),
       trailing: Icon(Icons.arrow_forward),
     );
+  }
+}
+
+class ClippingClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(
+      0,
+      size.height / 1.75,
+    ); //start path with this if you are making at bottom
+
+    var firstStart = Offset(size.width / 5, size.height);
+    //fist point of quadratic bezier curve
+    var firstEnd = Offset(size.width / 2.2, size.height - 50.0);
+    //second point of quadratic bezier curve
+    path.quadraticBezierTo(
+      firstStart.dx,
+      firstStart.dy,
+      firstEnd.dx,
+      firstEnd.dy,
+    );
+
+    var secondStart = Offset(
+      size.width - (size.width / 3.5),
+      size.height - 105,
+    );
+    //third point of quadratic bezier curve
+    var secondEnd = Offset(size.width, size.height / 1.4);
+    //fourth point of quadratic bezier curve
+    path.quadraticBezierTo(
+      secondStart.dx,
+      secondStart.dy,
+      secondEnd.dx,
+      secondEnd.dy,
+    );
+
+    path.lineTo(
+      size.width,
+      0,
+    ); //end with this path if you are making wave at bottom
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+    // TODO: implement shouldReclip
   }
 }
