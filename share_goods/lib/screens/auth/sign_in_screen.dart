b817af796a35514ed9f1489af69fef2a6766b0c4
Martin Rylund <martin@rylund.eu>
2021-01-17 00:51:23 +0100

    Added sign in form submission when using keyboard finish button

--- share_goods/lib/screens/auth/sign_in_screen.dart
+++ share_goods/lib/screens/auth/sign_in_screen.dart
@@ -197,7 +197,7 @@ class _SignInState extends State<SignIn> {
               ]),
           child: TextFormField(
               textInputAction: TextInputAction.done,
-              onFieldSubmitted: (_) => node.unfocus(),
+              onFieldSubmitted: (_) => logIn(),
               keyboardType: TextInputType.text,
               obscureText: true,
               style: TextStyle(color: Colors.white),
@@ -269,23 +269,23 @@ class _SignInState extends State<SignIn> {
                   fontWeight: FontWeight.bold,
                   fontFamily: 'OpenSans'),
             ),
-            onPressed: () async {
-              if (_formKey.currentState.validate()) {
-                dynamic result =
-                    await _auth.signInMail(email.trim(), password.trim());
-                print('Result is' + result);
-                if (result == null) {
-                  print('Result was null');
-                  setState(() => error = 'Indtast korrekt mail eller kode');
-                }
-                print('Email is:' + email);
-                print('Password is' + password);
-              } else {
-                setState(() => error = 'Indtast korrekt mail eller kode');
-              }
+            onPressed: () {
+              logIn();
             }),
       ),
     );
+  }
+
+  logIn() async {
+    if (_formKey.currentState.validate()) {
+      dynamic result =
+          await _auth.signInMail(email.trim(), password.trim());
+      if (result == null) {
+        setState(() => error = 'Indtast korrekt mail eller kode');
+      }
+    } else {
+      setState(() => error = 'Indtast korrekt mail eller kode');
+    }
   }
 
   Widget _buildRegisterButton() {
